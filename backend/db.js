c387c85cde32a281c63c209d83ed2511955bd8bd
nightraven16 <romelaugustine3@gmail.com>
2026-02-21 21:03:38 +0530

    chnged db to neon for local and deployment testing

--- backend/db.js
+++ backend/db.js
@@ -2,20 +2,8 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  port: Number(process.env.PGPORT),
+  connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false,
-  },
-}); 
-
-/* export const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "ecommerce",
-  password: "987654321",
-  port: 5432,
-}); */
+    rejectUnauthorized: false
+  }
+});
